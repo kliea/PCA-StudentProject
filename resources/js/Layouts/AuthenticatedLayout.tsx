fd7da8c9535ede8f1b2b7359119ec5a288fab491
Arbee <reniewritescode@gmail.com>
2024-11-25 20:20:50 +0800

    Added EmployeePageController

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -21,6 +21,9 @@ import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { navigationLinks } from "@/types/payroll";
 
+import logoImage from "../Components/images/Logo image.png"
+import logoText from "../Components/images/Logo text.png"
+
 export default function AuthenticatedLayoutEmployees({
     children,
     title,
@@ -35,7 +38,19 @@ export default function AuthenticatedLayoutEmployees({
         <SidebarProvider open={open} onOpenChange={() => setOpen(!open)}>
             <Sidebar variant="sidebar" collapsible="icon">
                 <SidebarHeader className="bg-baseGreen">
-                    <img src="https://placehold.co/600x400" alt="" />
+                        {/* <img src="https://placehold.co/600x400" alt="" /> */}
+    {/* 
+                        <div className="h-16 w-full bg-black">
+                            <img src={logoImage} alt="LogoImage" className="size-2/4" />
+                        </div> */}
+
+
+                        <div className="bg-opacity-60 flex items-center pt-2">
+                            <div className="flex size-3/4 items-center">
+                                <img src={logoImage} alt="LogoImage" className="max-w-[77px] max-h-[62px] group-data-[collapsible=icon]:w-9" />
+                                <img src={logoText} alt="logoText" className="max-w-[174.6px] max-h-[78.09px] group-data-[collapsible=icon]:hidden" />
+                            </div>
+                        </div>
                 </SidebarHeader>
                 <SidebarContent className="bg-baseGreen scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-white scrollbar-track-transparent overflow-y overflow-x-hidden">
                     {links.map((link) => (
