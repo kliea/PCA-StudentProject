46c8688cd4c4ccff62d630244d9eb756f46ce92a
Arbee <reniewritescode@gmail.com>
2024-11-25 19:06:43 +0800

    Added employee side

--- routes/web.php
+++ routes/web.php
@@ -17,58 +17,58 @@ Route::get('/', function () {
 
 
 // Route for storing SSL data
-// Route::post('/admin/ssl/store', [AdminPageController::class, 'ssl_addData'])->name('store.ssl');
+Route::post('/admin/ssl/store', [AdminPageController::class, 'ssl_addData'])->name('store.ssl');
 
-// Route::get('/admin/dashboard', function () {
-//     return Inertia::render('Admin/Dashboard');
-// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.dashboard');
+Route::get('/admin/dashboard', function () {
+    return Inertia::render('Admin/Dashboard');
+})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.dashboard');
 
-// Route::get('/admin/salary', function () {
-//     return Inertia::render('Admin/Salary');
-// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.salary');
+Route::get('/admin/salary', function () {
+    return Inertia::render('Admin/Salary');
+})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.salary');
 
-// Route::get('/admin/benefits', function () {
-//     return Inertia::render('Admin/Benefits');
-// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.benefits');
+Route::get('/admin/benefits', function () {
+    return Inertia::render('Admin/Benefits');
+})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.benefits');
 
-// Route::get('/admin/loans', function () {
-//     return Inertia::render('Admin/Loans');
-// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.loans');
+Route::get('/admin/loans', function () {
+    return Inertia::render('Admin/Loans');
+})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.loans');
 
-// Route::get('/admin/records', function () {
-//     return Inertia::render('Admin/Records');
-// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.records');
+Route::get('/admin/records', function () {
+    return Inertia::render('Admin/Records');
+})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.records');
 
-// Route::get('/admin/designations', function () {
-//     return Inertia::render('Admin/Designations');
-// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.designations');
+Route::get('/admin/designations', function () {
+    return Inertia::render('Admin/Designations');
+})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.designations');
 
-// Route::get('/admin/compensations', function () {
-//     return Inertia::render('Admin/Compensations');
-// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.compensations');
+Route::get('/admin/compensations', function () {
+    return Inertia::render('Admin/Compensations');
+})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.compensations');
 
-// Route::get('/admin/deductions', function () {
-//     return Inertia::render('Admin/Deductions');
-// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.deductions');
+Route::get('/admin/deductions', function () {
+    return Inertia::render('Admin/Deductions');
+})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.deductions');
 
 
 ////////////////////////////////bio routes
 
-Route::get('/bioadmin/dashboard', function () {
-    return Inertia::render('BioAdmin/Dashboard');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.dashboardb');
+// Route::get('/bioadmin/dashboard', function () {
+//     return Inertia::render('BioAdmin/Dashboard');
+// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.dashboardb');
 
-Route::get('/bioadmin/attendancelist', function () {
-    return Inertia::render('BioAdmin/AttendanceList');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.attendancelist');
+// Route::get('/bioadmin/attendancelist', function () {
+//     return Inertia::render('BioAdmin/AttendanceList');
+// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.attendancelist');
 
-Route::get('/bioadmin/attendancerecords', function () {
-    return Inertia::render('BioAdmin/AttendanceRecord');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.attendancerecords');
+// Route::get('/bioadmin/attendancerecords', function () {
+//     return Inertia::render('BioAdmin/AttendanceRecord');
+// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.attendancerecords');
 
-Route::get('/bioadmin/manageusers', function () {
-    return Inertia::render('BioAdmin/ManageUsers');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.manageusers');
+// Route::get('/bioadmin/manageusers', function () {
+//     return Inertia::render('BioAdmin/ManageUsers');
+// })->middleware('auth', 'verified', 'usercheck:admin')->name('admin.manageusers');
 
 // Empoyee Routes
 
@@ -105,27 +105,53 @@ Route::get('/bioadmin/manageusers', function () {
 // })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.deductions');
 
 
-// SUBDOMAIN FOR PAYROLL
-Route::domain('payroll.' . env('APP_URL'))->group(function () {
-    Route::prefix('admin')->group(function () {
-        Route::get('dashboard', [AdminPageController::class, 'index'])->name('admin.dashboard');
-        Route::get('payrolls', [AdminPageController::class, 'payrolls'])->name('admin.payrolls');
-        Route::get('loans', [AdminPageController::class, 'loans'])->name('admin.loans');
-        Route::get('employees', [AdminPageController::class, 'employees'])->name('admin.employees');
-        Route::get('compensations', [AdminPageController::class, 'compensations'])->name('admin.compensations');
-        Route::get('deductions', [AdminPageController::class, 'deductions'])->name('admin.deductions');
-        Route::get('governmentshare', [AdminPageController::class, 'governmentshare'])->name('admin.governmentshare');
-        Route::get('formats', [AdminPageController::class, 'format'])->name('admin.formats');
-        Route::get('appointments', [AdminPageController::class, 'appointments'])->name('admin.appointments');
-    });
+// // SUBDOMAIN FOR PAYROLL
+// Route::domain('payroll.' . env('APP_URL'))->group(function () {
+//     Route::prefix('admin')->group(function () {
+//         Route::get('dashboard', [AdminPageController::class, 'index'])->name('admin.dashboard');
+//         Route::get('payrolls', [AdminPageController::class, 'payrolls'])->name('admin.payrolls');
+//         Route::get('loans', [AdminPageController::class, 'loans'])->name('admin.loans');
+//         Route::get('employees', [AdminPageController::class, 'employees'])->name('admin.employees');
+//         Route::get('compensations', [AdminPageController::class, 'compensations'])->name('admin.compensations');
+//         Route::get('deductions', [AdminPageController::class, 'deductions'])->name('admin.deductions');
+//         Route::get('governmentshare', [AdminPageController::class, 'governmentshare'])->name('admin.governmentshare');
+//         Route::get('formats', [AdminPageController::class, 'format'])->name('admin.formats');
+//         Route::get('appointments', [AdminPageController::class, 'appointments'])->name('admin.appointments');
+//     });
 
-    // SSL CRUD
-    Route::prefix('admin')->group(function () {
-        Route::get('ssl', [SSLController::class, 'index'])->name('admin.ssl');
-        Route::post('ssl/store', [SSLController::class, 'store'])->name('store.ssl');
-        Route::put('/ssl/{salary_grade}', [SSLController::class, 'update'])->name('update.ssl');
-        Route::delete('/ssl/{salary_grade}', [SSLController::class,'destroy'])->name('delete.ssl');
-    });
+
+//     // SSL CRUD
+//     Route::prefix('admin')->group(function () {
+//         Route::get('ssl', [SSLController::class, 'index'])->name('admin.ssl');
+//         Route::post('ssl/store', [SSLController::class, 'store'])->name('store.ssl');
+//         Route::put('/ssl/{salary_grade}', [SSLController::class, 'update'])->name('update.ssl');
+//         Route::delete('/ssl/{salary_grade}', [SSLController::class,'destroy'])->name('delete.ssl');
+//     });
+// });
+
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('dashboard', [AdminPageController::class, 'index'])->name('admin.dashboard');
+    Route::get('payrolls', [AdminPageController::class, 'payrolls'])->name('admin.payrolls');
+    Route::get('loans', [AdminPageController::class, 'loans'])->name('admin.loans');
+    Route::get('employees', [AdminPageController::class, 'employees'])->name('admin.employees');
+    Route::get('compensations', [AdminPageController::class, 'compensations'])->name('admin.compensations');
+    Route::get('deductions', [AdminPageController::class, 'deductions'])->name('admin.deductions');
+    Route::get('governmentshare', [AdminPageController::class, 'governmentshare'])->name('admin.governmentshare');
+    Route::get('formats', [AdminPageController::class, 'format'])->name('admin.formats');
+    Route::get('appointments', [AdminPageController::class, 'appointments'])->name('admin.appointments');
+
+    // Route::get('ssl', [AdminPageController::class, 'ssl'])->name('admin.ssl');
+    // Route::post('ssl/store', [AdminPageController::class, 'ssl_addData'])->name('store.ssl');
+    // Route::put('/ssl/{salary_grade}', [AdminPageController::class, 'update'])->name('ssl.update');
+});
+
+// SSL CRUD
+Route::prefix('admin')->group(function () {
+    Route::get('ssl', [SSLController::class, 'index'])->name('admin.ssl');
+    Route::post('ssl/store', [SSLController::class, 'store'])->name('store.ssl');
+    Route::put('/ssl/{salary_grade}', [SSLController::class, 'update'])->name('update.ssl');
 });
 
 
