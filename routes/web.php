c171d8a0ef3d15bc36f2ce6950517c0092e98ef1
jlibdev <jlibandodev@gmail.com>
2024-12-07 09:36:16 +0800

    Merge Stage

--- routes/web.php
+++ routes/web.php
@@ -39,41 +39,6 @@ Route::get('/fetch-attendance', function () {
 // Route::put('/autogenerate-today', DTREntryController::create());
 Route::get('/autogenerate-today', [DailyTimeEntryController::class, 'create'])->name('generate-DTRs');
 
-// Route for storing SSL data
-Route::post('/admin/ssl/store', [AdminPageController::class, 'ssl_addData'])->name('store.ssl');
-
-Route::get('/admin/dashboard', function () {
-    return Inertia::render('Admin/Dashboard');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.dashboard');
-
-Route::get('/admin/salary', function () {
-    return Inertia::render('Admin/Salary');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.salary');
-
-Route::get('/admin/benefits', function () {
-    return Inertia::render('Admin/Benefits');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.benefits');
-
-Route::get('/admin/loans', function () {
-    return Inertia::render('Admin/Loans');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.loans');
-
-Route::get('/admin/records', function () {
-    return Inertia::render('Admin/Records');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.records');
-
-Route::get('/admin/designations', function () {
-    return Inertia::render('Admin/Designations');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.designations');
-
-Route::get('/admin/compensations', function () {
-    return Inertia::render('Admin/Compensations');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.compensations');
-
-Route::get('/admin/deductions', function () {
-    return Inertia::render('Admin/Deductions');
-})->middleware('auth', 'verified', 'usercheck:admin')->name('admin.deductions');
-
 
 // BIOmetrics routes
 
@@ -89,39 +54,6 @@ Route::get('/bioadmin/attendancerecords', function () {
     return Inertia::render('BioAdmin/AttendanceRecord');
 })->name('admin.attendancerecords');
 
-// Empoyee Routes
-
-// Route::get('/employee/dashboard', function () {
-//     return Inertia::render('Employee/Dashboard');
-// })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.dashboard');
-
-// Route::get('/employee/salary', function () {
-//     return Inertia::render('Employee/Salary');
-// })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.salary');
-
-// Route::get('/employee/benefits', function () {
-//     return Inertia::render('Employee/Benefits');
-// })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.benefits');
-
-// Route::get('/employee/loans', function () {
-//     return Inertia::render('Employee/Loans');
-// })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.loans');
-
-// Route::get('/employee/records', function () {
-//     return Inertia::render('Employee/Records');
-// })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.records');
-
-// Route::get('/employee/designations', function () {
-//     return Inertia::render('Employee/Designations');
-// })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.designations');
-
-// Route::get('/employee/compensations', function () {
-//     return Inertia::render('Employee/Compensations');
-// })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.compensations');
-
-// Route::get('/employee/deductions', function () {
-//     return Inertia::render('Employee/Deductions');
-// })->middleware('auth', 'verified', 'usercheck:employee')->name('employee.deductions');
 
 // SUBDOMAIN FOR PAYROLL
 Route::domain('payroll.' . env('APP_URL'))->group(function () {
